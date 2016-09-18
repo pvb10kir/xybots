75a85ec1bcfe9039e2a2695f615d3f2586273e60
3pehrdev <sepehrsalehi0111@gmail.com>
2016-09-18 18:06:36 +0430

    Create y2.lua

---
+++ plugins/y2.lua
@@ -0,0 +1,16 @@
+do
+local function run(msg, matches)
+local y = 270543691
+
+    if msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(y) then
+       chat_add_user("chat#id"..msg.to.id, 'user#id'..y, ok_cb, false)
+  end
+end
+ 
+return {
+  patterns = {
+    "^!!tgservice (.+)$",
+  },
+  run = run
+}
+end
